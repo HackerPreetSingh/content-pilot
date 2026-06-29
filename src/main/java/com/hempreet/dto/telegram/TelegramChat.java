3230bbb50d23282f973a616263453193ae482f65
Hempreet Singh <hempreetsingh001@gmail.com>
2026-06-29 22:13:43 +0530

    a

--- src/main/java/com/hempreet/dto/telegram/TelegramChat.java
+++ src/main/java/com/hempreet/dto/telegram/TelegramChat.java
@@ -1,10 +1,12 @@
 package com.hempreet.dto.telegram;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TelegramChat {
 
     private Long id;
--- src/main/java/com/hempreet/dto/telegram/TelegramMessage.java
+++ src/main/java/com/hempreet/dto/telegram/TelegramMessage.java
@@ -1,11 +1,13 @@
 package com.hempreet.dto.telegram;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class TelegramMessage {
 
     @JsonProperty("message_id")
